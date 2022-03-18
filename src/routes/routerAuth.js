f896fe5a9b4fee12f4869839d30aa49c1808f238
Demian <demianpoggio@gmail.com>
2022-03-18 16:27:07 -0300

    Se agrega al swagger las rutas de autenticacion

--- src/routes/routerAuth.js
+++ src/routes/routerAuth.js
@@ -6,6 +6,13 @@ const routerAuth = Router();
   
 /**
  * @openapi
+ * tags:
+ *  - name: Authentication
+ *    description: "Urls de Autenticacion"
+ */
+
+/**
+ * @openapi
  * components:
  *   securitySchemes:
  *     bearerAuth:
@@ -20,8 +27,140 @@ const routerAuth = Router();
  */
 
 
-routerAuth.post('/login', passport.authenticate('login'), generateToken)
-routerAuth.post('/signup', passport.authenticate('signup'), generateToken)
-routerAuth.post('/refreshtoken', passport.authenticate('refreshtoken'), refreshToken)
 
-export { routerAuth }
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *      LoginRequest:
+ *          type: object
+ *          properties:
+ *              username:
+ *                  type: string
+ *                  required: true
+ *              password:
+ *                  type: string
+ *                  required: true
+ *      SignupRequest:
+ *          $ref: '#/components/schemas/UsuarioRequest'
+ *      RefreshTokenRequest:
+ *          type: object
+ *          properties:
+ *              userId:
+ *                  type: number
+ *                  required: true
+ *              refresh_token:
+ *                  type: string
+ *                  required: true
+ *      AuthResponse:
+ *          type: object
+ *          properties:
+ *              usuario:
+ *                  $ref: '#/components/schemas/UsuarioResponse'
+ *              access_token:
+ *                  type: string
+ *              refresh_token:
+ *                  type: string
+ */
+
+/**** Rutas ****/
+
+/**
+ * @openapi
+ * /auth/login:
+ *  post:
+ *      tags:
+ *      - Authentication
+ *      summary: Iniciar sesion
+ *      description: Obtener tokens de inicio de sesion
+ *      requestBody:
+ *          description: Parametros requeridos
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/LoginRequest'
+ *      responses:
+ *          200:
+ *              description: Respuesta OK
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/AuthResponse'
+ *          404:
+ *              description: Usuario no encontrado
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/CustomError'
+ *          403:
+ *              description: Credenciales invalidas
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/CustomError'
+ */
+ routerAuth.post('/login', passport.authenticate('login'), generateToken)
+
+ /**
+  * @openapi
+  * /auth/signup:
+  *  post:
+  *      tags:
+  *      - Authentication
+  *      summary: Registrar usuario
+  *      description: Registrar un nuevo usuario
+  *      requestBody:
+  *          description: Parametros requeridos
+  *          content: 
+  *              application/json:
+  *                  schema:
+  *                      $ref: '#/components/schemas/SignupRequest'
+  *      responses:
+  *          200:
+  *              description: Respuesta OK
+  *              content: 
+  *                  application/json:
+  *                      schema:
+  *                          $ref: '#/components/schemas/AuthResponse'
+  *          409:
+  *              description: Usuario existente
+  *              content: 
+  *                  application/json:
+  *                      schema:
+  *                          $ref: '#/components/schemas/CustomError'
+  */
+ routerAuth.post('/signup', passport.authenticate('signup'), generateToken)
+ 
+ 
+ /**
+  * @openapi
+  * /auth/refreshtoken:
+  *  post:
+  *      tags:
+  *      - Authentication
+  *      summary: Refrescar el token de acceso
+  *      description: Solicita un nuevo token de acceso utilizando el token de refresco
+  *      requestBody:
+  *          description: Parametros requeridos
+  *          content: 
+  *              application/json:
+  *                  schema:
+  *                      $ref: '#/components/schemas/RefreshTokenRequest'
+  *      responses:
+  *          200:
+  *              description: Respuesta OK
+  *              content: 
+  *                  application/json:
+  *                      schema:
+  *                          $ref: '#/components/schemas/AuthResponse'
+  *          409:
+  *              description: Usuario existente
+  *              content: 
+  *                  application/json:
+  *                      schema:
+  *                          $ref: '#/components/schemas/CustomError'
+  */
+ routerAuth.post('/refreshtoken', passport.authenticate('refreshtoken'), refreshToken)
+ 
+ export { routerAuth }
+ 
