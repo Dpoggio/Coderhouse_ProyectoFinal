c1aefd28abb947fc9b8af4cd3c5ceed1ed820829
Demian <demianpoggio@gmail.com>
2022-03-23 21:04:01 -0300

    Se agrega validacion con duplicidad de contraseña

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,3 +1,17 @@
+const password = document.getElementById("password")
+const confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("La contraseña no coincide");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
 const formRegister = document.getElementById("formRegister");
 formRegister.addEventListener('submit', async e => {
 
